c2532f67996594625fec945a7583a9833ab1c6d5
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2018-02-26 14:49:32 -0300

    Projeto criado

---
+++ src/main/java/com/kaique/gerenciamentovendas/GerenciamentoVendasApplication.java
@@ -0,0 +1,12 @@
+package com.kaique.gerenciamentovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoVendasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kaique/gerenciamentovendas/GerenciamentoVendasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kaique.gerenciamentovendas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciamentoVendasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
