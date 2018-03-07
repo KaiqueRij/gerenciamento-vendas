0c01efb8ceaac19d2b3074b504aeb3bc4a6299b8
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2018-03-07 16:01:00 -0300

    Paginacao com parametros opcionais na requisicao.

--- src/main/java/com/kaique/gerenciamentovendas/services/CategoriaService.java
+++ src/main/java/com/kaique/gerenciamentovendas/services/CategoriaService.java
@@ -3,6 +3,9 @@ package com.kaique.gerenciamentovendas.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kaique.gerenciamentovendas.model.Categoria;
@@ -47,6 +50,11 @@ public class CategoriaService {
 		} catch (Exception e) {
 			throw new IntegridadeDaInformacaoException("Não é possível deletar categorias que possui produtos.");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return this.categoriaRepository.findAll(pageRequest);
 	}
 
 }
