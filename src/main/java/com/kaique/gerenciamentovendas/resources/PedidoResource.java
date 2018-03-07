84fe680854544e7657a972c1eb13e37ffe6dbff0
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2018-03-07 10:42:57 -0300

    Endpoint /pedidos/id disponibilizado.

---
+++ src/main/java/com/kaique/gerenciamentovendas/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.kaique.gerenciamentovendas.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kaique.gerenciamentovendas.model.Pedido;
+import com.kaique.gerenciamentovendas.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService pedidoService;
+	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<?> getPedidoById(@PathVariable Integer id){
+		Pedido pedido = this.pedidoService.getPedidoById(id);
+		return ResponseEntity.ok(pedido);
+	}
+	
+
+}
---
+++ src/main/java/com/kaique/gerenciamentovendas/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.kaique.gerenciamentovendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaique.gerenciamentovendas.model.Pedido;
+import com.kaique.gerenciamentovendas.repositorys.PedidoRepository;
+import com.kaique.gerenciamentovendas.services.exceptions.ObjetoNaoEncontradoException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	public Pedido getPedidoById(Integer id){
+		Pedido pedido = this.pedidoRepository.findOne(id);
+		
+		if(pedido == null){
+			throw new ObjetoNaoEncontradoException("Objeto não encontrado.");
+		}
+		
+		return pedido;
+	}
+
+}
