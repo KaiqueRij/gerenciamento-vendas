6bc0fc5eab0b3ccf2ade57caac2f589540f679e5
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2018-03-08 11:09:26 -0300

    Correcoes: update em CategoriaService e construtor de pagamento.

--- src/main/java/com/kaique/gerenciamentovendas/services/CategoriaService.java
+++ src/main/java/com/kaique/gerenciamentovendas/services/CategoriaService.java
@@ -40,8 +40,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj){
-		getCategoriaById(obj.getId());
-		return this.categoriaRepository.save(obj);		
+		Categoria newObj = getCategoriaById(obj.getId());
+		updateData(newObj, obj);
+		return this.categoriaRepository.save(newObj);		
 	}
 	
 	public void delete(Integer id){
@@ -61,5 +62,8 @@ public class CategoriaService {
 	public Categoria fromDto(CategoriaDTO objDto){
 		return new Categoria(objDto.getId(), objDto.getNome());		
 	}
-
+	
+	private void updateData(Categoria newObj, Categoria obj){
+		newObj.setNome(obj.getNome());
+	}
 }
