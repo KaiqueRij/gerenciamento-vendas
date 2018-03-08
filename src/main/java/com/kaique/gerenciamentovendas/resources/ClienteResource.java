27b7d632e2ce49275d4f089c347d67803940d589
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2018-03-08 11:00:43 -0300

    PUT, GET e DELETE para o cliente.

--- src/main/java/com/kaique/gerenciamentovendas/resources/ClienteResource.java
+++ src/main/java/com/kaique/gerenciamentovendas/resources/ClienteResource.java
@@ -1,12 +1,23 @@
 package com.kaique.gerenciamentovendas.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kaique.gerenciamentovendas.dtos.ClienteDTO;
 import com.kaique.gerenciamentovendas.model.Cliente;
 import com.kaique.gerenciamentovendas.services.ClienteService;
 
@@ -15,13 +26,43 @@ import com.kaique.gerenciamentovendas.services.ClienteService;
 public class ClienteResource {
 	
 	@Autowired
-	private ClienteService clientes;
+	private ClienteService clienteService;
 	
 	@GetMapping(value = "{id}")
 	public ResponseEntity<?> getClienteById(@PathVariable Integer id){
-		Cliente cliente = this.clientes.getClienteById(id);
+		Cliente cliente = this.clienteService.getClienteById(id);
 		return ResponseEntity.ok(cliente);
 	}
 	
-
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll(){
+		List<Cliente> list = this.clienteService.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
+		Page<Cliente> list = this.clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok(listDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = this.clienteService.fromDto(objDto);
+		obj.setId(id);
+		obj = this.clienteService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		this.clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/kaique/gerenciamentovendas/services/ClienteService.java
+++ src/main/java/com/kaique/gerenciamentovendas/services/ClienteService.java
@@ -1,20 +1,27 @@
 package com.kaique.gerenciamentovendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.kaique.gerenciamentovendas.dtos.ClienteDTO;
 import com.kaique.gerenciamentovendas.model.Cliente;
 import com.kaique.gerenciamentovendas.repositorys.ClienteRepository;
+import com.kaique.gerenciamentovendas.services.exceptions.IntegridadeDaInformacaoException;
 import com.kaique.gerenciamentovendas.services.exceptions.ObjetoNaoEncontradoException;
 
 @Service
 public class ClienteService {
 	
 	@Autowired
-	private ClienteRepository clientes;
+	private ClienteRepository clienteRepository;
 	
 	public Cliente getClienteById(Integer id){
-		Cliente categoria = this.clientes.findOne(id);
+		Cliente categoria = this.clienteRepository.findOne(id);
 		
 		if(categoria == null){
 			throw new ObjetoNaoEncontradoException("Objeto não encontrado.");
@@ -22,5 +29,37 @@ public class ClienteService {
 		
 		return categoria;
 	}
-
+	
+	public List<Cliente> findAll(){
+		return this.clienteRepository.findAll();
+	}
+	
+	public Cliente update(Cliente obj){
+		Cliente newObj = getClienteById(obj.getId());
+		updateData(newObj, obj);
+		return this.clienteRepository.save(newObj);		
+	}
+	
+	public void delete(Integer id){
+		getClienteById(id);
+		try {
+			this.clienteRepository.delete(id);			
+		} catch (Exception e) {
+			throw new IntegridadeDaInformacaoException("Não é possível excluir porque há entidades relacionadas.");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return this.clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDto){
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj){
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
