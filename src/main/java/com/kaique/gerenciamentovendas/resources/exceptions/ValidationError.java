e76fce2e520b96cdeeb1e646ab04cd3f4368ff0c
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2018-03-08 08:07:00 -0300

    Personalizando do retorno da validação.

---
+++ src/main/java/com/kaique/gerenciamentovendas/resources/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.kaique.gerenciamentovendas.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String mensagem, Long timeStamp) {
+		super(status, mensagem, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message){
+		this.errors.add(new FieldMessage(fieldName, message));
+	}
+}
