f89f2ca1a7f0e28b76fac9214e3652567ae6ffcc
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2019-12-23 12:39:43 -0300

    Envio de e-mail HTML

--- src/main/java/com/kaique/gerenciamentovendas/services/AbstractEmailService.java
+++ src/main/java/com/kaique/gerenciamentovendas/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.kaique.gerenciamentovendas.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.kaique.gerenciamentovendas.model.Pedido;
 
@@ -11,6 +19,12 @@ public abstract class AbstractEmailService implements EmailService {
 	
 	@Value("${default.sender}")
 	private String sender;
+	
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -29,5 +43,38 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setText(obj.toString());
 		
 		return sm;
+	}
+	
+	@Override
+	public void sendOrderConfirmationHtmlEmail (Pedido obj) {
+		
+		MimeMessage mm;
+		
+		try {
+			mm = prepareMimeMessageFromPedido(obj);
+			this.sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			this.sendOrderConfirmationEmail(obj);
+		}		
+	}
+	
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(this.sender);
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(this.htmlFromTemplatePedido(obj), true);
+		
+		return mimeMessage;
+	}
+
+	protected String htmlFromTemplatePedido (Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		return this.templateEngine.process("email/confirmacao-pedido", context);
 	}
 }
--- src/main/java/com/kaique/gerenciamentovendas/services/EmailService.java
+++ src/main/java/com/kaique/gerenciamentovendas/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.kaique.gerenciamentovendas.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.kaique.gerenciamentovendas.model.Pedido;
@@ -7,4 +9,7 @@ import com.kaique.gerenciamentovendas.model.Pedido;
 public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
