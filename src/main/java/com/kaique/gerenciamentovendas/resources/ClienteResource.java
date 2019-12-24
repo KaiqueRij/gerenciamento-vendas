841643ee29292eeab951ecdfd3e369477ee4b08f
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2019-12-24 14:53:10 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/kaique/gerenciamentovendas/resources/ClienteResource.java
+++ src/main/java/com/kaique/gerenciamentovendas/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,6 +39,7 @@ public class ClienteResource {
 		return ResponseEntity.ok(cliente);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<ClienteDTO>> findAll(){
 		List<Cliente> list = this.clienteService.findAll();
@@ -45,6 +47,7 @@ public class ClienteResource {
 		return ResponseEntity.ok(listDto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping(value = "/page")
 	public ResponseEntity<Page<ClienteDTO>> findPage(
 			@RequestParam(value = "page", defaultValue = "0") Integer page, 
@@ -64,6 +67,7 @@ public class ClienteResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
 		Cliente obj = this.clienteService.fromDto(objDto);
