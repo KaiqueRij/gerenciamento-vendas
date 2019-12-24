8e63ef9051d3d2845adc9368b3653d42c8347fdd
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2019-12-24 14:40:42 -0300

    Implementando autorizacao

--- src/main/java/com/kaique/gerenciamentovendas/config/SecurityConfig.java
+++ src/main/java/com/kaique/gerenciamentovendas/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.kaique.gerenciamentovendas.security.JWTAuthenticationFilter;
+import com.kaique.gerenciamentovendas.security.JWTAuthorizationFilter;
 import com.kaique.gerenciamentovendas.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(this.authenticationManager(), this.jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(this.authenticationManager(), this.jwtUtil, this.userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
