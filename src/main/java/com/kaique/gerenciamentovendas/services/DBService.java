7bce4e4954f126036354c1ca9347ffab0cb160ad
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2019-12-24 10:14:42 -0300

    Adicionando senha a Cliente

--- src/main/java/com/kaique/gerenciamentovendas/services/DBService.java
+++ src/main/java/com/kaique/gerenciamentovendas/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.kaique.gerenciamentovendas.model.Categoria;
@@ -32,6 +33,9 @@ import com.kaique.gerenciamentovendas.repositorys.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categorias;
@@ -111,7 +115,7 @@ public class DBService {
 		this.estadoRepository.save(Arrays.asList(est1, est2));
 		this.cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "kaiquerijkaardteste@gmail.com", "3333333", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "kaiquerijkaardteste@gmail.com", "3333333", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("232323", "242424"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "5544554", 
