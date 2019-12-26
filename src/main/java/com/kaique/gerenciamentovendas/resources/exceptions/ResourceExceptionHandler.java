e869757126d31bd1957e535c99f4182aa1801016
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2019-12-26 16:06:29 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/kaique/gerenciamentovendas/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/kaique/gerenciamentovendas/resources/exceptions/ResourceExceptionHandler.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.kaique.gerenciamentovendas.services.exceptions.AuthorizationException;
+import com.kaique.gerenciamentovendas.services.exceptions.FileException;
 import com.kaique.gerenciamentovendas.services.exceptions.IntegridadeDaInformacaoException;
 import com.kaique.gerenciamentovendas.services.exceptions.ObjetoNaoEncontradoException;
 
@@ -22,8 +26,7 @@ public class ResourceExceptionHandler {
 		
 		StandardError erro = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), 
 				System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
-		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);		
 	}
 	
 	@ExceptionHandler(IntegridadeDaInformacaoException.class)
@@ -32,8 +35,7 @@ public class ResourceExceptionHandler {
 		
 		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), 
 				System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
-		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);		
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -44,8 +46,7 @@ public class ResourceExceptionHandler {
 		for(FieldError x : e.getBindingResult().getFieldErrors()){
 			erro.addError(x.getField(), x.getDefaultMessage());
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
-		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);		
 	}
 	
 	@ExceptionHandler(AuthorizationException.class)
@@ -54,7 +55,42 @@ public class ResourceExceptionHandler {
 		
 		StandardError erro = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), 
 				System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);		
+	}
+	
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request){
 		
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), 
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);		
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, 
+			HttpServletRequest request){
+		
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+		StandardError erro = new StandardError(code.value(), e.getMessage(), 
+				System.currentTimeMillis());
+		return ResponseEntity.status(code).body(erro);		
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, 
+			HttpServletRequest request){
+		
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), 
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);		
+	}
+	
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, 
+			HttpServletRequest request){
+		
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), 
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);		
 	}
 }
