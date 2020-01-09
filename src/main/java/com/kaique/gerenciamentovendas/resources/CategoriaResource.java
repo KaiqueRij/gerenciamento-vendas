ac06ef287901b3c89f109d44ae61fcd79ad82a72
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2020-01-09 10:56:33 -0300

    Documentacao com Swagger - Parte 2

--- src/main/java/com/kaique/gerenciamentovendas/resources/CategoriaResource.java
+++ src/main/java/com/kaique/gerenciamentovendas/resources/CategoriaResource.java
@@ -25,6 +25,10 @@ import com.kaique.gerenciamentovendas.dtos.CategoriaDTO;
 import com.kaique.gerenciamentovendas.model.Categoria;
 import com.kaique.gerenciamentovendas.services.CategoriaService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
@@ -32,12 +36,14 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService categoriaService;
 	
+	@ApiOperation(value="Busca por id")
 	@GetMapping(value = "{id}")
 	public ResponseEntity<?> getCategoriaById(@PathVariable Integer id){
 		Categoria categoria = this.categoriaService.getCategoriaById(id);
 		return ResponseEntity.ok(categoria);
 	}
 	
+	@ApiOperation(value="Retorna todas as categorias")
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> list = this.categoriaService.findAll();
@@ -45,6 +51,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok(listDto);
 	}
 	
+	@ApiOperation(value="Retorna todas as categorias por paginação")
 	@GetMapping(value = "/page")
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
 			@RequestParam(value = "page", defaultValue = "0") Integer page, 
@@ -57,6 +64,7 @@ public class CategoriaResource {
 	}
 	
 	@PreAuthorize("hasAnyRole('ADMIN')")
+	@ApiOperation(value="Insere categoria")
 	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){
 		Categoria obj = this.categoriaService.fromDto(objDto);
@@ -66,6 +74,7 @@ public class CategoriaResource {
 	}
 	
 	@PreAuthorize("hasAnyRole('ADMIN')")
+	@ApiOperation(value="Atualiza categoria")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
 		Categoria obj = this.categoriaService.fromDto(objDto);
@@ -75,6 +84,10 @@ public class CategoriaResource {
 	}
 	
 	@PreAuthorize("hasAnyRole('ADMIN')")
+	@ApiOperation(value="Remove categoria")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Não é possível excluir uma categoria que possui produtos"),
+			@ApiResponse(code = 404, message = "Código inexistente") })
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		this.categoriaService.delete(id);
